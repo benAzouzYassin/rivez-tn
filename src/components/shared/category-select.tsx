ff82fef28d0881b54be91708061057afa1783864
yassinebenazouz123 <yassinebenazouz123@gmail.com>
2025-02-13 02:41:22 +0100

    fix: fix build errror

--- src/components/shared/category-select.tsx
+++ src/components/shared/category-select.tsx
@@ -42,7 +42,6 @@ export default function CategorySelect({
                 <AddCategoryDialog
                     isOpen={isAdding}
                     onOpenChange={setIsAdding}
-                    categoryName={nameToAdd}
                 />
             )}
         </>
